3666304aa6e6091a7b6c1a517c72a63cb7780f25
FILES=0 <files.eq.zero@gmail.com>
2019-03-28 10:43:43 +0900

    enable wordpress debug

--- testenv/wp/wp-config.php
+++ testenv/wp/wp-config.php
@@ -77,7 +77,10 @@ $table_prefix = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', false );
+// define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true);
+define( 'WP_DEBUG_DISPLAY', true);
+define( 'WP_DEBUG_LOG', false);
 
 // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
 // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
